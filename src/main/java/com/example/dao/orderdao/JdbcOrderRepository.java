07a1211b0795ea5edcc2850fa10200291f365d3d
javalover-design <2279818505@qq.com>
2022-04-20 00:53:20 +0800

    完善订单库处理

---
+++ src/main/java/com/example/dao/orderdao/JdbcOrderRepository.java
@@ -0,0 +1,66 @@
+package com.example.dao.orderdao;
+
+import com.example.pojo.Order;
+import com.example.pojo.Taco;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author lambda
+ */
+@Repository
+public class JdbcOrderRepository implements  OrderRepository{
+
+    private SimpleJdbcInsert orderInserter;
+    private SimpleJdbcInsert orderTacoInserter;
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public JdbcOrderRepository(JdbcTemplate jdbc) {
+        this.orderInserter=new SimpleJdbcInsert(jdbc).withTableName("Taco_Order")
+                .usingGeneratedKeyColumns("id");
+        this.orderTacoInserter=new SimpleJdbcInsert(jdbc).withTableName("Taco_Order_Tacos");
+        this.objectMapper=new ObjectMapper();
+    }
+
+    @Override
+    public Order save(Order order) {
+        order.setPlacedAt(new Date());
+        long orderId=saveOrderDetails(order);
+        order.setId(orderId);
+
+        return order;
+    }
+
+    /**
+     * 传入一个订单信息，返回订单的id
+     * @param order
+     * @return
+     */
+    private long saveOrderDetails(Order order){
+        @SuppressWarnings("unchecked")
+        Map<String,Object>values=objectMapper.convertValue(order,Map.class);
+        values.put("placedAt",order.getPlacedAt());
+        long orderId=orderInserter.executeAndReturnKey(values).longValue();
+        return orderId;
+    }
+
+    /**
+     * 保存内容到订单中
+     * @param taco
+     * @param orderId
+     */
+    private void saveTacoToOrder(Taco taco,long orderId){
+        Map<String, Object>values=new HashMap<>();
+        values.put("tacoOrder",orderId);
+        values.put("taco",taco.getId());
+        orderTacoInserter.execute(values);
+    }
+}
